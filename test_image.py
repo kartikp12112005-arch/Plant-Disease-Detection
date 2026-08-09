4d88ef8a34d9d6cfdc6e13e669bd6290e786e483
Kartik Pangavhane <kartikp12112005@gmail.com>
2026-08-09 23:45:26 +0530

    Reformat test_image.py for consistency

--- test_image.py
+++ test_image.py
@@ -2,10 +2,8 @@ import tensorflow as tf
 import numpy as np
 from tensorflow.keras.utils import load_img, img_to_array
 
-# Load trained model
 model = tf.keras.models.load_model("model/plant_disease_model.h5")
 
-# Class names (ORDER SAME AS TRAINING)
 class_names = [
     'Apple_Healthy',
     'Apple_Black_Rot',
@@ -15,12 +13,10 @@ class_names = [
     'Tomato_Late_Blight'
 ]
 
-# Load image (CORRECT PATH)
 img = load_img("test_images/test.jpg", target_size=(224,224))
 img = img_to_array(img) / 255.0
 img = np.expand_dims(img, axis=0)
 
-# Predict
 prediction = model.predict(img)
 predicted_class = class_names[np.argmax(prediction)]
 
